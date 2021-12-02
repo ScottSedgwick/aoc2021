5b57c676661fc09bb1cfc0035a266937819faf38
Scott Sedgwick <ssedgwick@cochlear.com>
2021-12-02 11:33:13 +1100

    Added Data.List import for zip4, and removed hand-rolled version

--- src/Day01.hs
+++ src/Day01.hs
@@ -1,5 +1,6 @@
 module Day01 (Model, parser, pt1, pt2) where
 
+import Data.List (zip4)
 import ParseUtils ( Parser, intline )
 import Text.Megaparsec ( many )
 
@@ -17,10 +18,3 @@ pt2 :: Model -> Int
 pt2 xs = foldr (\(a,b,c,d) r -> r + if ((a + b + c)::Int) < ((b + c + d)::Int) then 1 else 0) 0 ys
   where
     ys = zip4 xs (drop 1 xs) (drop 2 xs) (drop 3 xs)
-    
-zip4 :: [a] -> [a] -> [a] -> [a] -> [(a,a,a,a)]
-zip4 [] _ _ _ = []
-zip4 _ [] _ _ = []
-zip4 _ _ [] _ = []
-zip4 _ _ _ [] = []
-zip4 (w:ws) (x:xs) (y:ys) (z:zs) = (w,x,y,z) : zip4 ws xs ys zs
