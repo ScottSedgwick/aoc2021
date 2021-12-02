92c01fe9a903812d085607ba5ffcd1b810032b74
Scott Sedgwick <ssedgwick@cochlear.com>
2021-12-02 18:11:16 +1100

    Tidied up Day 02

--- src/Day02.hs
+++ src/Day02.hs
@@ -1,65 +1,49 @@
 module Day02 (Model, parser, pt1, pt2) where
 
+import Data.List (foldl')
 import Text.Megaparsec ( (<|>), many )
 import Text.Megaparsec.Char ( string )
 import ParseUtils ( Parser, intline ) 
 
 data Model = Forward Int
            | Down Int
-           | Up Int 
-           | WTF
+           | Up Int
            deriving stock Show
 
 parser :: Parser [Model]
 parser = many modelParser
 
 modelParser :: Parser Model
-modelParser = do
-  dn <- pDirn
-  ds <- intline 
-  let mdl | dn == "forward " = Forward ds
-          | dn == "down "    = Down ds
-          | dn == "up "      = Up ds
-          | otherwise        = WTF
-  pure mdl
+modelParser = pModel "forward " Forward
+          <|> pModel "down " Down
+          <|> pModel "up " Up
 
-pDirn :: Parser String
-pDirn = string "forward "
-    <|> string "up "
-    <|> string "down "
+pModel :: String -> (Int -> Model) -> Parser Model
+pModel s m = string s >> m <$> intline
+
+data Sub1 = Sub1
+  { sub1Forward :: Int
+  , sub1Depth :: Int
+  } deriving stock Show
 
 pt1 :: [Model] -> Int
-pt1 xs = forwards xs * (downs xs - ups xs)
+pt1 = g . foldl' f (Sub1 { sub1Forward = 0, sub1Depth = 0 })
+  where
+    f s (Forward x) = s { sub1Forward = sub1Forward s + x }
+    f s (Down x)    = s { sub1Depth   = sub1Depth   s + x }
+    f s (Up x)      = s { sub1Depth   = sub1Depth   s - x }
+    g s = sub1Forward s * sub1Depth s
 
-forwards :: [Model] -> Int
-forwards []             = 0
-forwards (Forward x:xs) = x + forwards xs
-forwards (_:xs)         = forwards xs
-
-downs :: [Model] -> Int
-downs []          = 0
-downs (Down x:xs) = x + downs xs
-downs (_:xs)      = downs xs
-
-ups :: [Model] -> Int
-ups []        = 0
-ups (Up x:xs) = x + ups xs
-ups (_:xs)    = ups xs
-
-data Submarine = Submarine
-  { aim :: Int
-  , fwd :: Int
-  , depth :: Int
+data Sub2 = Sub2
+  { sub2Aim :: Int
+  , sub2Forward :: Int
+  , sub2Depth :: Int
   } deriving stock Show
 
 pt2 :: [Model] -> Int
-pt2 = pt2' (Submarine { aim = 0, fwd = 0, depth = 0})
-
-pt2' :: Submarine -> [Model] -> Int
-pt2' s [] = fwd s * depth s
-pt2' s (x:xs) = pt2' (f x) xs
+pt2 = g . foldl' f (Sub2 { sub2Aim = 0, sub2Forward = 0, sub2Depth = 0})
   where
-    f (Forward y) = s { fwd = fwd s + y, depth = depth s + aim s * y }
-    f (Down y)    = s { aim = aim s + y }
-    f (Up y)      = s { aim = aim s - y }
-    f _           = s
+    f s (Forward y) = s { sub2Forward = sub2Forward s + y, sub2Depth = sub2Depth s + sub2Aim s * y }
+    f s (Down y)    = s { sub2Aim = sub2Aim s + y }
+    f s (Up y)      = s { sub2Aim = sub2Aim s - y }
+    g s             = sub2Forward s * sub2Depth s
