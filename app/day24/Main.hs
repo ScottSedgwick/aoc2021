6dbd9ff87f66e9ce999f08461cace2c0d80a9588
Scott Sedgwick <ssedgwick@cochlear.com>
2021-12-27 20:02:56 +1100

    Completed Day 24

--- app/day24/Main.hs
+++ app/day24/Main.hs
@@ -1,21 +1,10 @@
 module Main (main) where
 
-import Day24 
-import ParseUtils (parseFromFile)
-import System.Environment (getArgs)
+import Day24 ( parser, pt1, pt2 )
+import Data.Char ( intToDigit )
 
 main :: IO ()
 main = do
-  args <- getArgs
-  let inputfile = head args
-  let stage = head $ tail args
-  result <- parseFromFile parser inputfile
-  case result of
-    Left e -> do
-      putStrLn "Error: " 
-      print e
-    Right xs -> do
-      let x = if stage == "1"
-              then pt1 xs
-              else pt2 xs
-      print x
+  steps <- parser "data/day24/data.txt"
+  putStrLn $ map intToDigit $ pt1 steps
+  putStrLn $ map intToDigit $ pt2 steps
--- src/Day24.hs
+++ src/Day24.hs
@@ -1,14 +1,49 @@
-module Day24 (Model, parser, pt1, pt2) where
+module Day24 (Model, Step, parser, pt1, pt2) where
 
-import ParseUtils ( Parser )
+import Control.Monad.State ( MonadState(get), State, evalState, modify )
+import Data.List.Split ( chunksOf )
+import qualified Data.Set as S
 
-type Model = [Int]
+type Step = (Int, Int, Int)
+type Model = [Step]
 
-parser :: Parser Model
-parser = undefined
+parser :: String -> IO Model
+parser filename = map parseStep . chunksOf 18 . map words . lines <$> readFile filename
 
-pt1 :: Model -> Int
-pt1 = undefined
+parseStep :: [[String]] -> Step
+parseStep input = (read x, read y, read z)
+  where
+    x = input !! 5 !! 2
+    y = input !! 4 !! 2
+    z = input !! 15 !! 2
 
-pt2 :: Model -> Int
-pt2 = undefined
+-- Part 1
+
+solve :: [Int] -> Int -> Model -> State (S.Set (Int, Int)) [[Int]]
+solve _ _ [] = pure []
+solve ws z (s : steps) = do
+  dp <- get
+  if S.member (z, length steps) dp
+    then pure []
+    else do
+      result <- concat <$> traverse (\w -> f w (step z s w)) ws
+      modify (S.insert (z, length steps))
+      pure result
+  where
+    f w z' = if null steps
+             then (if z' == 0 then pure [[w]] else pure [])
+             else map (w :) <$> solve ws (step z s w) steps
+
+step :: Integral p => p -> (p, p, p) -> p -> p
+step z (addX, divZ, addY) w = if x /= w then (z' * 26) + (w + addY) else z'
+  where
+    x  = (z `rem` 26) + addX
+    z' = z `quot` divZ 
+
+pt1 :: Model -> [Int]
+pt1 steps = head $ evalState (solve [9, 8 .. 1] 0 steps) S.empty
+
+-- Part 2
+
+pt2 :: Model -> [Int]
+pt2 steps = head $ evalState (solve [1 .. 9] 0 steps) S.empty
